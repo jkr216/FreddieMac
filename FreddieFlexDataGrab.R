436a64ca2d8078e1865795db261acd74b2197b51
jkr216 <jonathan.regenstein@rstudio.com>
2016-09-15 03:57:44 +0000

    added some comments

--- FreddieFlexDataGrab.R
+++ FreddieFlexDataGrab.R
@@ -22,35 +22,24 @@ Metros_Only_500k<-Metros_Only_500k[!grepl(", PR", Metros_Only_500k$NAME),]
 Metros_Only_500k <-Metros_Only_500k[order(Metros_Only_500k$NAME),]
 
 ##get the metro county data sheet 1, remove first 4 rows and last blank rows
-msas_A_L <- read_excel("~/FreddieMac/metros.xls", 
+msas_AL <- read_excel("msas_new.xls", 
                        sheet = "MSA Indices A-L", skip = 4)
-colnames(msas_A_L)=msas_A_L[1,]
-msas_A_L=msas_A_L[c(-1, -495:-551),]
+colnames(msas_AL)=msas_AL[1,]
+msas_AL=msas_AL[c(-1, -500:-555),]
 
-msas_new_AL <- read_excel("msas_new.xls", 
-                       sheet = "MSA Indices A-L", skip = 4)
-colnames(msas_new_AL)=msas_new_AL[1,]
-msas_new_AL=msas_new_AL[c(-1, -500:-555),]
-
-msas_new_MZ <- read_excel("msas_new.xls", 
+msas_MZ <- read_excel("msas_new.xls", 
                           sheet = "MSA Indices M-Z", skip = 4)
-colnames(msas_new_MZ)=msas_new_MZ[1,]
-msas_new_MZ=msas_new_MZ[c(-1, -500:-516),-1]
+colnames(msas_MZ)=msas_MZ[1,]
+msas_MZ=msas_MZ[c(-1, -500:-516),-1]
 
-##get the metro county data sheet 2, remove first 4 rows, blank last AND
-##remove the date column
-msas_M_Z <- read_excel("~/FreddieMac/metros.xls", 
-                       sheet = "MSA Indices M-Z", skip = 4)
-colnames(msas_M_Z)=msas_M_Z[1,]
-msas_M_Z=msas_M_Z[c(-1, -495:-512),-1]
 
 ##combine metros areas into one dataframe and conver to numeric
-metros_All <- cbind(msas_A_L,msas_M_Z)
+metros_All <- cbind(msas_AL,msas_MZ)
 cols = c(2:382)    
 metros_All[,cols] = apply(metros_All[,cols], 2, function(x) as.numeric(as.character(x)))
 
 ##get rid of the cycle column, make it into a date and rename it 'date'
-metros_All$Month<-seq(mdy('01/01/1975'),mdy('02/01/2016'),by='months')
+metros_All$Month<-seq(mdy('01/01/1975'),mdy('06/01/2016'),by='months')
 colnames(metros_All)[1] <- "date"
 
 metros_gathered<- metros_All %>% 
@@ -66,40 +55,35 @@ metroFirstWord<-word(colnames(metros_All)[2:382], 1, sep = "-")
 metroFirstWord<-word(metroFirstWord, 1, sep = ",")
 colnames(metros_All)[2:382]<-paste(metroFirstWord,StateAbb,sep=" ")
 
+##create a data frame with the last year of hpi's
+##and an hpa column
+Metros <-  metros_All %>%  
+  filter(date == ymd("2016-06-01") | date == ymd("2015-06-01")) %>%
+  gather(metro, value, -date) %>%
+  spread(date, value) %>% 
+  mutate(hpa = (((1+(.[[3]]-.[[2]])/.[[2]])^1)-1)*100) %>%
+  mutate(hpa = round(hpa, digits =2))
+
+##add hpa to the shapefile
+Metros_Only_500k$hpa <- round(Metros$hpa, digits = 2)
+
+##calculate the pre 08 max, then add the diff from that max to today to the shapefile
 metros_01_08<- metros_All %>%  
   gather("metro", "maxHPI", -date) %>% 
   filter(date > "2000-01-01" & date <"2008-06-01") %>%
   group_by(metro) %>% 
   top_n(1, maxHPI) 
   
-Metros_Only_500k$CurrentDiff <- round(MetrosHpa$`2016-01-01` - metros_01_08$maxHPI, digits =2)
+Metros_Only_500k$Pre08MaxDiff <- round(Metros$`2016-06-01` - metros_01_08$maxHPI, digits =2)
 
+##calculate 08 to 15 trough, then add the diff from that trough to today to the shapefile
+metros_08_to_15<- metros_All %>%  
+  gather("metro", "minHPI", -date) %>% 
+  filter(date > "2008-01-01" & date <"2015-03-01") %>%
+  group_by(metro) %>% 
+  top_n(-1, minHPI) 
 
-##add HPA to shapefile
-Metros_Only_500k$hpaTest <-  
-  
-  metros_All %>%  
-  filter(date == ymd("2016-01-01") | date == ymd("2015-01-01")) %>%
-  gather(metro, value, -date) %>%
-  spread(date, value) %>% 
-  mutate(hpa = (((1+(.[[3]]-.[[2]])/.[[2]])^1)-1)*100) %>%
-  transmute(hpa = round(hpa, digits =2)) 
-
-Metros_Only_500k$hpa <- round(MetrosHpa$hpa, digits = 2)
-
-
-
-##misc
-metros_MinPostCrash <- metros_melted %>% group_by(metro) %>% 
-  filter(date > "2008-01-01" & date <"2016-02-01") %>%
-  top_n(-1, hpi)
-
-df$maxDiffPre2008 <- df$Year2 - metros_MaxHpi$hpi
-df$troughDiffPost2008 <- df$Year2 - metros_MinPostCrash$hpi
-
-Metros_Only_500k$percentChange08Peak <- round(df$maxDiffPre2008, digits = 2)
-Metros_Only_500k$percentChange08_16Trough <- round(df$troughDiffPost2008, digits = 2)
-
+Metros_Only_500k$PostCrashTroughDiff <- round(Metros$`2016-06-01` - metros_08_to_15$minHPI, digits =2)
 
 
 ###################STATES########################
@@ -151,9 +135,8 @@ states_00_08<- states %>%
 
 states500k$PeakDiff <- round(StatesHpa$`2016-03-01` - states_00_08$maxHPI, digits =2)
 
-
 ##create an xts object. why? Dygraphs needs one, that's why.
 metros_All_xts <-xts(metros_All, order.by=as.Date(metros_All$date))
 states_xts <-xts(states, order.by = as.Date(states$date))
 ### Save
-save(metros_All_xts, Metros_Only_500k, states_xts, states500k, dfmetro, metros_All, file = 'SourceData.RDat')
+save(metros_All_xts, Metros_Only_500k, states_xts, states500k, metros_All, file = 'SourceData.RDat')
